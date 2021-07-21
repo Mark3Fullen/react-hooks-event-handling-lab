957a21af9b169c62140fbac5026df19fecb1df86
Mark <mark3fullen@gmail.com>
2021-07-21 19:00:28 -0400

    commit tree 9556b519e72487de4d6534168f0bdbd68bc6d396

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    function bruh(event) {
+        console.log('Good!')
+    }
+
+    function bruhg() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <>
+            <button onFocus={bruh} onBlur={bruhg}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
